6c329e235804fe961ca0651a7e1529a2f8d98866
pratikac <pratik.ac@gmail.com>
2012-09-25 19:21:35 -0400

    timepass

--- c/test.cpp
+++ c/test.cpp
@@ -3,17 +3,12 @@
 #include "sarsop.h"
 
 using namespace pomdp;
-using namespace sarsop;
 
 int main()
 {
     //test_mymath();
     
     Model m = create_model();
-    Solver s = Solver(m);
-    s.initialize();
-    
-    s.solve(0.1);
     
     return 0;
 }
