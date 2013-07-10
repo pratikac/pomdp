26c3b866d73bf31fcd6a02c613915a94ba1e4f83
pratik <pratik.ac@gmail.com>
2013-07-10 16:07:17 -0400

    wip on lightdark

--- c/system/lightdark.h
+++ c/system/lightdark.h
@@ -3,9 +3,89 @@
 
 #include "../system.h"
 
-class lightdark_t : public system_t
+template<size_t ns, size_t nu, size_t no>
+class lightdark_t : public system_t<ns, nu, no>
 {
   public:
+    vector<region_t<ns> > light_regions;
+
+    lightdark_t()
+    {
+      ns = dim;
+      nu = dim;
+      no = dim;
+    }
+    
+    vec sample_state()
+    {
+      vec s = Zero(ns);
+      region_t<ns>& r = operating_region;
+      float p = RANDF;
+      if(p < 0.1)
+      {
+        r = goal_region;
+      }
+      else if(p < 0.4)
+      {
+        int p2 = RANDF*light_regions.size();
+        r = light_regions[p2];
+      }
+      for(int i=0; i< ns; i++)
+        s(i) = r.c(i) + r.s(i)*(RANDF-0.5);
+      return s;
+    }
+    vec sample_control()
+    {
+      vec u = Zero(nu);
+      region_t<nu>& r = control_region;
+      for(int i=0; i< ns; i++)
+        u(i) = r.c(i) + r.s(i)*(RANDF-0.5);
+      return u;
+    }
+    vec sample_observation()
+    {
+      vec o = sample_state();
+      return get_observation(o);
+    }
+    vec get_observation(const vec& s)
+    {
+      return s;
+    }
+    vec get_key(const vec& s)
+    {
+      vec k = s - operating_region.c;
+      for(int i=0; i<ns; i++)
+        k(i) = k(i)/operating_region.s(i) + 0.5;
+      return k;
+    }
+    vec get_fdt(const vec& s, const vec& u, float dt=1.0)
+    {
+      return u*dt;
+    }
+    vec get_FFdt(const vec& s, const vec& u, float dt=1.0)
+    {
+      return Identity(ns,ns)*0.1;
+    }
+    mat get_GG(const vec& s)
+    {
+      bool is_light = false;
+      for(auto& r : light_regions)
+      {
+        if(r.is_inside(s))
+        {
+          is_light = true;
+          break;
+        }
+      }
+      if(is_light)
+        return Identity(no,no)*0.01;
+      else
+        return Identity(no,no)*100;
+    }
+    float get_ht(const vec& s, const vec& u, const float r)
+    {
+      return r*r/(r*get_fdt(s,u,1).norm() + get_FFdt(s,u,1).norm());
+    }
 
 };
 
--- c/system/system.h
+++ c/system/system.h
@@ -1,13 +1,72 @@
 #ifndef __system_h__
 #define __system_h__
 
+#include "../linalg.h"
+#include "../utils.h"
+
+#define RANDF   (rand()/(RAND_MAX+1.0))
+
+template<size_t dim> class region_t;
+
+template<size_t ns, size_t nu, size_t no>
 class system_t{
   public:
-    /// dimension of states, controls, observations
-    int ns, nu, no;
-
+    
+    region_t<ns> operating_region, goal_region;
+    region_t<nu> control_region;
+    region_t<no> observation_region;
+    vector<region_t<ns> > obstacles;
+    
     system_t();
     ~system();
+
+    bool is_in_goal(vec& s)
+    {
+      return goal_region.is_inside(s);
+    }
+
+    bool is_in_obstacle(vec& s)
+    {
+      for(auto& r : obstacles)
+      {
+        if(r.is_inside(s))
+          return true;
+      }
+      return false;
+    }
+    
+    virtual vec sample_state() = 0;
+    virtual vec sample_control() = 0;
+    virtual vec sample_observation() = 0;
+    virtual vec sample_observation(const vec& s) = 0;
+    virtual vec get_key(const vec& s) = 0;
+
+    virtual vec get_fdt(const vec& s, const vec& u, float dt=1.0) = 0;
+    virtual vec get_FFdt(const vec& s, const vec&u, float dt=1.0) = 0;
+    virtual mat get_GG(const vec& s) = 0;
+    virtual float get_ht(const vec& s, const vec& u, const float r) = 0;
 };
 
+template<size_t dim>
+class region_t{
+  public:
+    vec c;
+    vec s;
+    region_t(vec& c_in, vec& s_in)
+    {
+      c = c_in;
+      s = s_in;
+    }
+    bool is_inside(vec& state)
+    {
+      vec diff = (state - c)/2.0;
+      for(int i=0; i<dim; i++)
+      {
+        if( (diff(i) > s(i)/2) || (diff(i) < -s(i)/2) )
+          return false;
+      }
+      return true;
+    }
+
+};
 #endif
