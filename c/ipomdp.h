7e19ea414a4f2adee367ee17cb72800c5f9071e4
pratik <pratik.ac@gmail.com>
2013-07-11 11:34:43 -0400

    wip

--- c/ipomdp.h
+++ c/ipomdp.h
@@ -2,21 +2,23 @@
 #define __ipomdp_h__
 
 #include "model.h"
+#include "pbvi.h"
 #include "system/lightdark.h"
 
-template<class system_t>
+template<class system_t, class solver_t>
 class ipomdp_t{
   public:
+    int ns, nu, no;
 
     system_t system;
-    ipomdp_t()
-    {
-      
-    }
-    ~ipomdp_t()
-    {
+    solver_t solver;
 
+    ipomdp_t(){
+      ns = 10;
+      nu = 2.0*log(ns);
+      no = nu;
     }
+    ~ipomdp_t(){};
 
 };
 
